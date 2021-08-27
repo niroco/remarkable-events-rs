3639c4aec3b647dd3e659e02484a6823214bf11e
Niclas Rosengren <niclas.rosengren@gmail.com>
2021-08-27 20:14:51 +0200

    build script read from arg

--- build-and-deploy.rs
+++ build-and-deploy.rs
@@ -6,13 +6,13 @@ TARGET="root@192.168.1.192"
 ARCH="armv7-unknown-linux-gnueabihf"
 
 echo "Compiling..."
-cross build --target "$ARCH" --release --example print_events || exit 1
+cross build --target "$ARCH" --release --example $1 || exit 1
 echo "Done"
 
-echo "Copying target/$ARCH/release/examples/print_events to $TARGET:~/"
-scp target/$ARCH/release/examples/print_events $TARGET:~/
+echo "Copying target/$ARCH/release/examples/$1 to $TARGET:~/"
+scp target/$ARCH/release/examples/$1 $TARGET:~/
 echo "Done"
 
-echo "Making binary executable"
-ssh $TARGET "chmod +x remlabs"
-echo "Done"
+#echo "Making binary executable"
+#ssh $TARGET "chmod +x remlabs"
+#echo "Done"
